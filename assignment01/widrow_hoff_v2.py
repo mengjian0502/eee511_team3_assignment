a158ad3768bf0e14e849fd73a2b70cc7d8875371
Jian Meng <jianmeng@Jians-MacBook-Pro.local>
2020-02-02 00:40:36 -0700

    Change the number of epoch to 1. Successfully implemented LMS. The prediction is saved  in the default save path.

--- assignment01/widrow_hoff_v2.py
+++ assignment01/widrow_hoff_v2.py
@@ -30,6 +30,7 @@ def main():
     else:
         print(f'save path already exist')
 
+
     train_target_path = './mackey-glass/data/mg_series_5000.npy'           # dataset (time series)
 
     weights = np.zeros([args.time_step, 1])                                # initialize the weight vector as a t_step by 1 vector
@@ -42,30 +43,12 @@ def main():
     print(f"number of samples for training: {train_target.shape}")
     print(f"number of samples for test: {test_target.shape}")
 
-
     for epoch in range(args.epochs):
-        train_pred, weights, train_loss, train_mse = train(args.eta, train_target, weights)
-        test_pred, test_loss, test_mse = test(test_target, weights, epoch, args.save_path)
+        weights, train_mse = train(args.eta, train_target, weights)
+        # test_mse = test(test_target, weights, epoch, args.save_path)
 
-        print(f'[Epoch: {epoch+1}], training error: {train_mse}, test loss: {test_mse}')
+        print(f'[Epoch: {epoch+1}], training error: {train_mse}')
 
-    plt.figure(figsize=(8,6))
-    
-    plt.plot(range(dataset.shape[0]), dataset, linewidth=2, label='original time series')
-    plt.plot(range(train_target.shape[0]-args.time_step), train_pred, linestyle='-.', label='training prediction')
-    plt.plot(range(train_target.shape[0], train_target.shape[0] + test_target.shape[0]-args.time_step), test_pred, linestyle=':', label='test prediction')
-
-    plt.title(f'LMS algorithm with MG time series: \n training average loss {train_mse[0]}, test average loss: {test_mse[0]}')
-    plt.xlabel('time')
-    plt.ylabel('$x(t)$')
-
-    plt.legend(loc='best')
-
-    plt.savefig(args.save_path+'prediction.png', dpi=300)
-
-    plt.close()
-
-        
 
 def dataloader(target_path):
     """
@@ -83,9 +66,6 @@ def train(eta, target, weight):
     """
     loss_sum = 0
 
-    series_pred = []
-    series_loss = []
-
     for ii in range(target.shape[0]-args.time_step):  
         
         t_start, t_end = ii, ii + args.time_step
@@ -96,20 +76,14 @@ def train(eta, target, weight):
         y_pred = weight.T @ x
         loss = (y_true - y_pred)
 
-        if ii % 50 == 0:
-            print(f'training loss: {loss}')
-
         weight = weight + eta * loss * x
 
         loss_sum += loss
-
-        series_pred.append(y_pred[0])
-        series_loss.append(loss[0])
     
     mse = loss_sum / target.shape[0]
 
 
-    return np.array(series_pred), weight, np.array(series_loss), mse
+    return weight, mse
 
 
 def test(target, weight, epoch, save_path):
@@ -117,9 +91,6 @@ def test(target, weight, epoch, save_path):
     LMS algorithm for Mackey Glass Time Series prediction
     """
     loss_sum = 0
-
-    series_pred = []
-    series_loss = []
 
     for ii in range(target.shape[0]-args.time_step): 
         
@@ -131,19 +102,24 @@ def test(target, weight, epoch, save_path):
         y_pred = weight.T @ x
         loss = (y_true - y_pred)
 
-        if ii % 50 == 0:
-            print(f'test loss: {loss}')
-
         loss_sum += loss
-
-        series_pred.append(y_pred[0])
-        series_loss.append(loss[0])
 
     mse = loss_sum / target.shape[0]
 
-    return np.array(series_pred), np.array(series_loss), mse
+    return mse
 
 
+def test_plot(epoch, y_true, y_pred, loss):
+    print(y_pred.shape)
+    plt.figure(figsize=(8,6))   # default figure size: (8,6)
+    plt.plot(range(y_true.shape[0]), y_true, linewidth=2, label='true time series (test)')
+    plt.plot(np.arange(0, y_true.shape[0], args.time_step), y_pred, linestyle='-.', label='predicted time series (test)')
+    plt.title(f"LMS test: epoch: {epoch}, mse: {loss}", fontsize=14, fontweight='bold')
+    plt.xlabel('time')
+    plt.ylabel('$x(t)$')
+
+    plt.legend(loc='best')
+    plt.savefig(args.save_path+f'test_epoch{epoch}.png', dpi=300)
 
 if __name__ == '__main__':
     main()
